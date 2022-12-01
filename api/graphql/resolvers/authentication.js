2f89529c64e7ebebc7a1af0d64299ea350cdc1f6
Saranoja <Saranoja@users.noreply.github.com>
2022-12-01 19:40:51 +0200

    chore: prettier & format files

--- api/graphql/resolvers/authentication.js
+++ api/graphql/resolvers/authentication.js
@@ -5,54 +5,50 @@ const jwt = require('jsonwebtoken');
 const realmApi = new RealmApiClient();
 
 const getTokenByEmailAndPassword = async (_, args, context) => {
-	try {
-		const { access_token, refresh_token, user_id } =
-			await realmApi.getEmailPasswordAccessToken(args.email, args.password);
-		const accessTokenExpiry = new Date(jwt.decode(access_token).exp * 1000);
-		const refreshTokenExpiry = new Date(jwt.decode(refresh_token).exp * 1000);
+  try {
+    const { access_token, refresh_token, user_id } = await realmApi.getEmailPasswordAccessToken(
+      args.email,
+      args.password
+    );
+    const accessTokenExpiry = new Date(jwt.decode(access_token).exp * 1000);
+    const refreshTokenExpiry = new Date(jwt.decode(refresh_token).exp * 1000);
 
-		context.res
-			.cookie('access_token', access_token, {
-				expires: accessTokenExpiry,
-				httpOnly: true,
-				sameSite: 'lax',
-				domain:
-					process.env.NODE_ENV === 'development'
-						? 'localhost'
-						: process.env.COOKIE_DOMAIN,
-				secure: process.env.NODE_ENV === 'development' ? false : true,
-			})
-			.cookie('refresh_token', refresh_token, {
-				expires: refreshTokenExpiry,
-				httpOnly: true,
-				sameSite: 'lax',
-				domain:
-					process.env.NODE_ENV === 'development'
-						? 'localhost'
-						: process.env.COOKIE_DOMAIN,
-				secure: process.env.NODE_ENV === 'development' ? false : true,
-			});
+    context.res
+      .cookie('access_token', access_token, {
+        expires: accessTokenExpiry,
+        httpOnly: true,
+        sameSite: 'lax',
+        domain: process.env.NODE_ENV === 'development' ? 'localhost' : process.env.COOKIE_DOMAIN,
+        secure: process.env.NODE_ENV === 'development' ? false : true,
+      })
+      .cookie('refresh_token', refresh_token, {
+        expires: refreshTokenExpiry,
+        httpOnly: true,
+        sameSite: 'lax',
+        domain: process.env.NODE_ENV === 'development' ? 'localhost' : process.env.COOKIE_DOMAIN,
+        secure: process.env.NODE_ENV === 'development' ? false : true,
+      });
 
-		return { access_token, refresh_token, user_id };
-	} catch (e) {
-		throw new AuthenticationError(e);
-	}
+    return { access_token, refresh_token, user_id };
+  } catch (e) {
+    throw new AuthenticationError(e);
+  }
 };
 
 const assertAuthenticated = (context) => {
-	try {
-		return RealmApiClient.getUserFromGraphQL(context.token);
-	} catch (e) {
-		throw e;
-	}
+  try {
+    return RealmApiClient.getUserFromGraphQL(context.token);
+  } catch (e) {
+    throw e;
+  }
 };
 
 module.exports = {
-	queries: {
-		getTokenByEmailAndPassword,
-	},
-	mutations: {},
-	helper: {
-		assertAuthenticated,
-	},
+  queries: {
+    getTokenByEmailAndPassword,
+  },
+  mutations: {},
+  helper: {
+    assertAuthenticated,
+  },
 };
--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const firebaseAdmin = require('firebase-admin');
 const serviceAccount = require('./api/config/service-account.json'); /*-> This refers to the google cloud service account, [Learn How to Get it From](https://firebase.google.com/docs/admin/setup)*/
 
 firebaseAdmin.initializeApp({
-	credential: firebaseAdmin.credential.cert(serviceAccount),
-	databaseURL: 'https://squad-b2c2b.firebaseio.com/',
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: 'https://squad-b2c2b.firebaseio.com/',
 });
 
 const database = firebaseAdmin.firestore();
@@ -25,79 +25,69 @@ const settings = { timestampsInSnapshots: true };
 database.settings(settings);
 
 const startServer = async () => {
-	app.use(
-		cors({
-			origin: (origin, callback) => {
-				const whitelist = [
-					'http://localhost:8000',
-					'http://localhost:3000',
-					'https://squad-demos.netlify.app',
-					'https://thelookbook.io',
-				];
-				if (whitelist.indexOf(origin) !== -1 || !origin) {
-					callback(null, true);
-				} else {
-					callback(new Error('Not allowed by CORS'));
-				}
-			},
-			allowedHeaders: [
-				'Origin',
-				'X-Requested-With',
-				'Content-Type',
-				'Accept',
-				'Authorization',
-			],
-			methods: ['POST', 'GET'],
-			exposedHeaders: ['Set-Cookie'],
-			credentials: true,
-			optionsSuccessStatus: 200,
-		})
-	);
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        const whitelist = [
+          'http://localhost:8000',
+          'http://localhost:3000',
+          'https://squad-demos.netlify.app',
+          'https://thelookbook.io',
+        ];
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+      methods: ['POST', 'GET'],
+      exposedHeaders: ['Set-Cookie'],
+      credentials: true,
+      optionsSuccessStatus: 200,
+    })
+  );
 
-	app.use(express.urlencoded({ extended: false }));
-	app.use(logger.pre);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(logger.pre);
 
-	const server = new ApolloServer({
-		context: async ({ req, res }) => {
-			const token = req.headers.authorization || '';
-			return { token, res, req };
-		},
-		modules: [
-			require('./api/graphql/modules/user'),
-			require('./api/graphql/modules/customer'),
-			require('./api/graphql/modules/brand'),
-			require('./api/graphql/modules/category'),
-			require('./api/graphql/modules/uploadPhoto'),
-			require('./api/graphql/modules/product'),
-			require('./api/graphql/modules/compensation'),
-			require('./api/graphql/modules/notification'),
-			require('./api/graphql/modules/analytics'),
-			require('./api/graphql/modules/authentication'),
-		],
-	});
+  const server = new ApolloServer({
+    context: async ({ req, res }) => {
+      const token = req.headers.authorization || '';
+      return { token, res, req };
+    },
+    modules: [
+      require('./api/graphql/modules/user'),
+      require('./api/graphql/modules/customer'),
+      require('./api/graphql/modules/brand'),
+      require('./api/graphql/modules/category'),
+      require('./api/graphql/modules/uploadPhoto'),
+      require('./api/graphql/modules/product'),
+      require('./api/graphql/modules/compensation'),
+      require('./api/graphql/modules/notification'),
+      require('./api/graphql/modules/analytics'),
+      require('./api/graphql/modules/authentication'),
+    ],
+  });
 
-	server.applyMiddleware({
-		app,
-		path: '/api/graphql',
-		// cors logic handled in the `cors()` middleware
-		cors: false,
-	});
+  server.applyMiddleware({
+    app,
+    path: '/api/graphql',
+    // cors logic handled in the `cors()` middleware
+    cors: false,
+  });
 
-	app.listen({ port: process.env.PORT }, () => {
-		dbClient
-			.connect()
-			.then((client) => {
-				console.log(
-					'Apollo Server on http://localhost:' +
-						process.env.PORT +
-						'/api/graphql'
-				);
-				console.log('MongoDB Connected');
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	});
+  app.listen({ port: process.env.PORT }, () => {
+    dbClient
+      .connect()
+      .then((client) => {
+        console.log('Apollo Server on http://localhost:' + process.env.PORT + '/api/graphql');
+        console.log('MongoDB Connected');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
 
 startServer();
