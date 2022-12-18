91f5386dc5511f6e1563d633cf713a7ff71ea60f
Veronica Nisenbaum <veroyv412@gmail.com>
2022-12-18 16:26:31 +0100

    add logs for DB

--- api/config/mongo.js
+++ api/config/mongo.js
@@ -4,16 +4,13 @@ const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-const uri = process.env.BD_URL;
+const uri = process.env.MONGO_BD_URL;
 const dbClient = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
-
-console.log('process.env.BD_URL', process.env.BD_URL);
-console.log('Backend URI', uri);
-console.log('dbClient', dbClient);
-
 const dbName = process.env.DB_NAME;
 
+console.log('process.env.MONGO_BD_URL', process.env.MONGO_BD_URL);
+console.log('Backend URI', uri);
+console.log('dbClient', dbClient);
 console.log('dbName', dbName);
 
 module.exports.dbClient = dbClient;
