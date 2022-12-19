1111371526d189ee76deeb48efbd1ef1c08a38a7
Saranoja <Saranoja@users.noreply.github.com>
2022-12-19 13:12:14 +0200

    feat: add username to registration

--- api/graphql/resolvers/authentication.js
+++ api/graphql/resolvers/authentication.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server');
-const { RealmApiClient } = require('../../utils/Realm')
+const { RealmApiClient } = require('../../utils/Realm');
 const realmApi = new RealmApiClient();
 const { dbClient, dbName } = require('../../config/mongo');
 const sgMail = require('@sendgrid/mail');
@@ -44,58 +44,60 @@ const assertAuthenticated = (context) => {
   }
 };
 
-const registerUser = async (parent, args) => {
-    try {
+const registerUser = async (_, args) => {
+  try {
+    const { email, password, displayName, username } = args.user;
 
-        const { email, password, displayName, role } = args.user;
-        const realmUser = await realmApi.registerUser(email, password);
+    const realmUser = await realmApi.registerUser(email, password);
+    const currentDate = new Date();
 
-        let user = {
-            stitchId: realmUser.id,
-            displayName: displayName,
-            role: role ? role : 'member',
-            email: email,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        };
+    let user = {
+      stitchId: realmUser.id,
+      displayName: displayName,
+      role: 'member',
+      email: email,
+      username: username,
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    };
 
-        await dbClient.db(dbName).collection('users').insertOne(user);
+    await dbClient.db(dbName).collection('users').insertOne(user);
 
-        let token = jwt.sign({id: realmUser.id}, 'squadConfirmationEmailHashThatIsSuperSecure', { expiresIn: '1h' })
+    let token = jwt.sign({ id: realmUser.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
 
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        const msg = {
-            to: user.email,
-            from: {
-                name: "The Lookbook Team",
-                email: "fred@teammysquad.com"
-            },
-            templateId: "d-b4712b8325e74eab98976c4ba0bcd5b9",
-            dynamic_template_data: {
-                link: process.env.FRONTEND_URL + `confirm-email/${token}`,
-                name: user.displayName
-            }
-        };
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-        console.log(msg)
+    const msg = {
+      to: user.email,
+      from: {
+        name: 'The Lookbook Team',
+        email: 'fred@teammysquad.com',
+      },
+      templateId: 'd-b4712b8325e74eab98976c4ba0bcd5b9',
+      dynamic_template_data: {
+        link: process.env.FRONTEND_URL + `confirm-email/${token}`,
+        name: user.displayName,
+      },
+    };
 
-        await sgMail.send(msg);
+    await sgMail.send(msg);
 
-        return await realmApi.getEmailPasswordAccessToken(email, password)
-    } catch (e){
-        throw e;
-    }
-}
-
+    return true;
+  } catch (e) {
+    throw e;
+  }
+};
 
 module.exports = {
-    queries: {
-        getTokenByEmailAndPassword
-    },
-    mutations: {
-        registerUser
-    },
-    helper: {
-        assertAuthenticated
-    }
-}
+  queries: {
+    getTokenByEmailAndPassword,
+  },
+  mutations: {
+    registerUser,
+  },
+  helper: {
+    assertAuthenticated,
+  },
+};
